40e1aeb0de5dbbb8589acbddfd17380ec8e73993
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-16 17:52:42 -0400

    test: evaluateRegex should return the regular expression when it is safe

--- test/util.test.js
+++ test/util.test.js
@@ -1,1 +1,17 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import { evaluateRegex } from '../src/util.js';
+
+describe('util test suite', () => {
+  describe('evaluateRegex', () => {
+    it('should return the regular expression when it is safe', () => {
+      const safeRegex = /^safe$/;
+
+      const result = evaluateRegex(safeRegex);
+
+      expect(result).to.be.deep.equal(safeRegex);
+    });
+  });
+});
