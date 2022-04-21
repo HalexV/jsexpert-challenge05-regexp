b736aaf4a4163d546c81ae38fee730625b9d32d0
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-21 16:44:46 -0400

    feat: build should return the content

--- src/textProcessorFluentAPI.js
+++ src/textProcessorFluentAPI.js
@@ -1,1 +1,12 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+export default class TextProcessorFluentAPI {
+  #content;
+
+  constructor(content) {
+    this.#content = content;
+  }
+
+  build() {
+    return this.#content;
+  }
+}
