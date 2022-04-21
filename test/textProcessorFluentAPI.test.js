104210eac3c577eded2ab8eba154eb872464f6d0
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-21 16:45:08 -0400

    test: build should return the content

--- test/textProcessorFluentAPI.test.js
+++ test/textProcessorFluentAPI.test.js
@@ -1,1 +1,29 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+
+import TextProcessorFluentAPI from '../src/textProcessorFluentAPI.js';
+
+/*
+  Passos:
+  1) ler o arquivo CSV
+  2) O csv é passado para o textProcessorFluentAPI
+  3) Separar os headers do content
+  4) Separar os contents por linhas projeto
+  5) Transformar cada linha em um raw object
+  6) Passar cada raw object para classe Project e criar uma lista de projects
+  7) Retornar os projects
+*/
+
+describe('textProcessorFluentAPI Suite Test', () => {
+  describe('build', () => {
+    it('should return the content', () => {
+      const mockContent = 'any';
+      const sut = new TextProcessorFluentAPI(mockContent);
+
+      const result = sut.build();
+
+      expect(result).to.be.deep.equal(mockContent);
+    });
+  });
+});
