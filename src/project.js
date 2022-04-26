64ff9a4679a4d0362d1acd97051606cf9f20bcc8
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-26 09:41:31 -0400

    feat: project class should return numero and ano fields from titulo field

--- src/project.js
+++ src/project.js
@@ -1,1 +1,13 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+import { evaluateRegex } from './util.js';
+
+export default class Project {
+  constructor({ titulo, link, autor, etapa, ementa, indexadoresnorma }) {
+    [this.numero, this.ano] = titulo.match(evaluateRegex(/\d+/g));
+    this.link = link;
+    this.autor = autor;
+    this.etapa = etapa;
+    this.ementa = ementa;
+    this.indexadoresnorma = indexadoresnorma;
+  }
+}
