dab524dfd934d70a59a576b24d017bf7b1710778
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-26 10:04:04 -0400

    feat: project class should return an empty autores array field when autor field has no data

--- src/project.js
+++ src/project.js
@@ -4,8 +4,12 @@ import { evaluateRegex } from './util.js';
 export default class Project {
   constructor({ titulo, link, autor, etapa, ementa, indexadoresnorma }) {
     [this.numero, this.ano] = titulo.match(evaluateRegex(/\d+/g));
-    this.link = link;
-    this.autor = autor;
+    [this.id] = link.match(evaluateRegex(/(?<=id=)\d+/));
+
+    if (autor === 'no data') {
+      this.autores = [];
+    }
+
     this.etapa = etapa;
     this.ementa = ementa;
     this.indexadoresnorma = indexadoresnorma;
