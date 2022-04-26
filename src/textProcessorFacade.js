df612369feeec1649f0b83c5685bf92b00cfbb6d
Halex Viotto Gomes <halexx002@gmail.com>
2022-04-26 16:39:38 -0400

    feat: getProjectsFromCSV should call the functions in the correct order

--- src/textProcessorFacade.js
+++ src/textProcessorFacade.js
@@ -1,1 +1,18 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+
+export default class TextProcessorFacade {
+  #textProcessorFluentAPI;
+
+  constructor({ TextProcessorFluentAPI, text }) {
+    this.#textProcessorFluentAPI = new TextProcessorFluentAPI(text);
+  }
+
+  getProjectsFromCSV() {
+    return this.#textProcessorFluentAPI
+      .extractHeadersFromContent()
+      .splitContent()
+      .makeRawObjects()
+      .makeProjects()
+      .build();
+  }
+}
